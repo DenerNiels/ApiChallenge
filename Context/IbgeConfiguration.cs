a6746909b45f87664e0a65210129cd281984715a
Dener Niels <ksin_niels@outlook.com>
2024-03-11 07:31:35 +0000

    Configuration

--- Context/IbgeConfiguration.cs
+++ Context/IbgeConfiguration.cs
@@ -1,6 +1,26 @@
-﻿namespace ApiChallenge.Context
+﻿using ApiChallenge.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiChallenge.Context
 {
-    public class IbgeConfiguration
+    public class IbgeConfiguration : IEntityTypeConfiguration<Ibge>
     {
+        public void Configure(EntityTypeBuilder<Ibge> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Id)
+                .IsUnique();
+
+            builder.Property(x => x.City)
+                .IsRequired();
+
+            builder.Property(x => x.State)
+                .IsRequired();
+        }
     }
 }
--- Context/UserConfiguration.cs
+++ Context/UserConfiguration.cs
@@ -1,6 +1,31 @@
-﻿namespace ApiChallenge.Context
+﻿using ApiChallenge.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiChallenge.Context
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+
+            builder.Property(x => x.Password)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Ignore(x => x.AcessToken);
+        }
     }
 }
